8b2d8b8dbeaf2611316ab2bf6d202bd6e8cc2e93
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-07-15 11:29:10 -0400

    Refactors plugin author

--- patchchat.php
+++ patchchat.php
@@ -3,8 +3,8 @@
 Plugin Name: PatchChat
 Version: 0.1-alpha
 Description: A chat plugin for WordPress
-Author: Casey Patrick Driscoll
-Author URI: https://patch.works/about/casey
+Author: PatchWorks
+Author URI: https://patch.works/
 Plugin URI: https://patch.works/plugin/patchchat
 Text Domain: patchworks
 Domain Path: /languages
