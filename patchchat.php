135e0d80b25673118d68caa64add978b848de797
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-07-15 11:28:13 -0400

    Init commit

---
+++ patchchat.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: PatchChat
+Version: 0.1-alpha
+Description: A chat plugin for WordPress
+Author: Casey Patrick Driscoll
+Author URI: https://patch.works/about/casey
+Plugin URI: https://patch.works/plugin/patchchat
+Text Domain: patchworks
+Domain Path: /languages
+*/
+
