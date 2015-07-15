159621b6335e3524a693b3cfec466aa8df8aa95d
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-07-15 15:37:09 -0400

    Adds interactions for chat box

--- assets/jsx/front.jsx
+++ assets/jsx/front.jsx
@@ -1,10 +1,17 @@
 jQuery( document ).ready( function() {
 
-	jQuery( 'body' ).append( '<div id="patchchat">' );
+	jQuery( 'body' )
+		.append( '<div class="patchchat"><header>PatchChat</header><section><textarea></textarea></section></div>' )
+		.find( '.patchchat header' )
+		.on( 'click', function() {
+			jQuery( '.patchchat > section' ).toggle();
+			jQuery( '.patchchat textarea' ).focus();
+		} );
 
-	React.render(
-		<h1>Hi, there!</h1>,
-		document.getElementById( 'patchchat' )
-	);
+	jQuery( '.patchchat' ).delegate( 'textarea', 'keyup', function (){
+		jQuery( this ).height( 0 );
+		jQuery( this ).height( this.scrollHeight );
+	});
+	jQuery( '.patchchat' ).find( 'textarea' ).keyup();
 
 } );
