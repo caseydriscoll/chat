82420b30d0b166ae0904af38ac7c69377c1615d7
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-08-27 17:52:01 -0400

    Refactors to fix menu count bugs

--- assets/js/admintable.js
+++ assets/js/admintable.js
@@ -1,5 +1,13 @@
 var prevstatus = '';
 
+function add_status_menu( status ) {
+	var statusProper = status.charAt(0).toUpperCase() + status.slice(1);
+	var menuItem = '<li class="' + status + '"><a href="edit.php?post_status=' + status + '&amp;post_type=patchchat">' + statusProper + ' <span class="count">(1)</span></a></li>'
+	
+	jQuery( '.subsubsub li' ).last().append( '| ' );
+	jQuery( '.subsubsub' ).append( menuItem );
+}
+
 jQuery( document ).ready( function() {
 
 	jQuery( 'select' )
@@ -33,12 +41,22 @@ jQuery( document ).ready( function() {
 					jQuery( '#post-' + chat_id ).find( 'img' ).hide();
 
 					// Increment the new post_status
-					count = jQuery( '.' + post_status ).find( '.count' ).html().replace(/[()]/g, '');
-					jQuery( '.' + post_status ).find( '.count' ).html( '(' + ++count + ')' );
+					var menu = jQuery( '.subsubsub .' + post_status );
+
+					if ( menu.length == 0 ) {
+						add_status_menu( post_status );
+					} else {
+						count = jQuery( '.subsubsub .' + post_status ).find( '.count' ).html().replace( /[()]/g, '' );
+						jQuery( '.' + post_status ).find( '.count' ).html( '(' + ++count + ')' );
+					}
 
 					// Decrement the old prev_status
-					count = jQuery( '.' + prev_status ).find( '.count' ).html().replace(/[()]/g, '');
+					count = jQuery( '.' + prev_status ).find( '.count' ).html().replace( /[()]/g, '' );
 					jQuery( '.' + prev_status ).find( '.count' ).html( '(' + --count + ')' );
+
+					if ( count == 0 ) {
+						jQuery( '.subsubsub .' + prev_status ).remove();
+					}
 
 					jQuery( '#post-' + chat_id )
 						.removeClass( 'status-' + prev_status )
@@ -48,11 +66,10 @@ jQuery( document ).ready( function() {
 						jQuery( '#post-' + chat_id ).fadeOut( 'normal', function () {
 							jQuery( this ).remove();
 						} );
-					}
 
-
-					if ( count == 0 ) {
-						jQuery( '#the-list' ).append( '<tr class="no-items"><td class="colspanchange" colspan="6">No Chats found</td></tr>' );
+						if ( count == 0 ) {
+							jQuery( '#the-list' ).append( '<tr class="no-items"><td class="colspanchange" colspan="6">No Chats found</td></tr>' );
+						}
 					}
 
 				}
